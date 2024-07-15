0cc5c3f6f3481af9491ab6bf7523d1e7a7ca269c
PedroCunha06 <pdh.alves@outlook.com>
2024-07-15 20:39:43 -0300

    Config models Request, RequestItem

--- request/admin.py
+++ request/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from request.models import Request, RequestItem
 
-# Register your models here.
+class RequestItemInline(admin.TabularInline):
+    model = RequestItem
+    extra = 1
+
+
+class RequestAdmin(admin.ModelAdmin):
+    inlines = [
+        RequestItemInline
+    ]
+    list_display = 'id', 'user', 'status'  # Mostrar name no display
+    ordering = '-id', # Ordernar por Id decrescente
+    search_fields = 'name', 'id',  # Campo de pesquisa
+    list_per_page = 10  # Itens por página
+    list_max_show_all = 100 # Itens máximo a se mostrar na tela
+    list_editable = 'status',   # Permite ter tópicos editáveis
+    list_display_links = 'user',   # O que está como editable, não pode estar aqui
+    
+admin.site.register(Request, RequestAdmin)  # Registra o model no admin
--- request/models.py
+++ request/models.py
@@ -42,9 +42,16 @@ class Request(models.Model):
             ('F', 'Finalizado'),
         )
     )
+    
+    def __str__(self):
+        return f'Pedido Nº {self.pk}'
 
 
 class RequestItem(models.Model):
+    class Meta:
+        verbose_name = 'RequestItem'   # O que mostrar quando houver uma categoria
+        verbose_name_plural = 'RequestItens'  # O que mostrar quando estiver no plural
+        
     request = models.ForeignKey(Request, on_delete=models.CASCADE)
     product = models.CharField(max_length=255)
     product_id = models.PositiveIntegerField()
@@ -54,3 +61,7 @@ class RequestItem(models.Model):
     promocional_price = models.FloatField(default=0)
     quantity = models.PositiveIntegerField()
     image = models.CharField(max_length=2000)
+    
+    def __str__(self):
+        return f'Item do {self.request}'
+    
