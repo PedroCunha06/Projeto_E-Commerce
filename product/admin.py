d56742efc28a1946a19889b97c0df0f12d7534af
PedroCunha06 <pdh.alves@outlook.com>
2024-07-15 13:10:02 -0300

    Config model product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from product.models import Product
 
-# Register your models here.
+@admin.register(Product)    # Usa o decorator para registrar o model
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'marketing_price'    # Mostrar name no display
+    ordering = '-id', # Ordernar por Id decrescente
+    search_fields = 'name', 'id',  # Campo de pesquisa
+    list_per_page = 10  # Itens por página
+    list_max_show_all = 100 # Itens máximo a se mostrar na tela
+    list_editable = 'marketing_price',   # Permite ter tópicos editáveis
+    list_display_links = 'name',   # O que está como editable, não pode estar aqui
--- product/models.py
+++ product/models.py
@@ -1,4 +1,9 @@
+# from django.conf import settings
 from django.db import models
+from django.conf import settings
+from PIL import Image
+import os
+
 
 '''
 Produto:
@@ -22,15 +27,16 @@ Produto:
             estoque - Int
 '''
 
-class Produto(models.Model):
-	nome = models.CharField(max_length=255)
-	descricao_curta = models.TextField(max_length=255)
-	descricao_longa = models.TextField()
-	imagem = models.ImageField(upload_to='pictures/%Y/%m', blank=True, null=True)
+
+class Product(models.Model):
+	name = models.CharField(max_length=255)
+	short_descripion = models.TextField(max_length=255)
+	long_description = models.TextField()
+	image = models.ImageField(upload_to='pictures/%Y/%m', blank=True, null=True)
 	slug = models.SlugField(unique=True)
-	preco_marketing = models.FloatField()
-	preco_marketing_promocional = models.FloatField(default=0)
-	tipo = models.CharField(
+	marketing_price = models.FloatField()
+	promocional_marketing_price = models.FloatField(default=0)
+	type_product = models.CharField(
 		default='V',
 		max_length=1,
 		choices=(
@@ -38,3 +44,34 @@ class Produto(models.Model):
 				('S', 'Simples'),
 			)
 		)
+
+	# Função de Redimensionamento de imagem.
+	@staticmethod
+	def resize_image(img, new_width=800):
+		image_full_path = os.path.join(settings.MEDIA_ROOT, img.name)	# Pega caminho da imagem
+		img_pil = Image.open(image_full_path)	# Usa o Pillow para abrir a imagem
+		original_widht, original_height = img_pil.size
+
+		if original_widht > new_width:
+			new_height = round((new_width * original_height) / original_widht)	# Calcula altura da imagem, mantendo proporção
+			new_image = img_pil.resize((new_width, new_height), Image.LANCZOS)	# Cria nova imagem, com nova largura e altura.
+			new_image.save(		# Salva imagem no mesmo lugar que a anterior, porém com novas dimensões
+					image_full_path,
+					optimize = True,
+					quality=90
+				)
+
+		img_pil.close()
+
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
+
+		max_image_size = 800
+
+		if self.image:
+			self.resize_image(self.image, max_image_size)
+
+
+	def __str__(self):
+		return self.name
+
