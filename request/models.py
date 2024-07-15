dbc0f9f98f1fae0cd407beb8168e9e7870edc214
PedroCunha06 <pdh.alves@outlook.com>
2024-07-15 20:30:22 -0300

    Create model RequestItem

--- request/models.py
+++ request/models.py
@@ -42,3 +42,15 @@ class Request(models.Model):
             ('F', 'Finalizado'),
         )
     )
+
+
+class RequestItem(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    product = models.CharField(max_length=255)
+    product_id = models.PositiveIntegerField()
+    variation = models.CharField(max_length=255)
+    variation_id = models.PositiveIntegerField()
+    price = models.FloatField()
+    promocional_price = models.FloatField(default=0)
+    quantity = models.PositiveIntegerField()
+    image = models.CharField(max_length=2000)
