0b50e92ce1b15aa8a761ffe273d51cc8940e049b
PedroCunha06 <pdh.alves@outlook.com>
2024-07-15 20:20:36 -0300

    Create model Request

--- request/models.py
+++ request/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+"""
+    Pedido:
+        user - FK User
+        total - Float
+        status - Choices
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+
+        ItemPedido:
+            pedido - FK pedido
+            produto - Char
+            produto_id - Int
+            variacao - Char
+            variacao_id - Int
+            preco - Float
+            preco_promocional - Float
+            quantidade - Int
+            imagem - Char
+
+"""
+
+
+class Request(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        default='C',
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        )
+    )
