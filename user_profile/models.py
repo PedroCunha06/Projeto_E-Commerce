09d6aca0d553eb9c09577d19232028a7b017a263
PedroCunha06 <pdh.alves@outlook.com>
2024-07-16 19:17:30 -0300

    Create model ProfileUser

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+'''
+PerfilUsuario
+        user - FK user (ou OneToOne)
+        idade - Int
+        data_nascimento - Date
+        cpf - char
+        endereco - char
+        numero - char
+        complemento - char
+        bairro - char
+        cep - Char
+        cidade - char
+        estado - Choices
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+
+'''
+
+class ProfileUser(models.Model):
+    user = models.models.OneToOneField(User, on_delete=models.CASCADE)()
+    age = models.PositiveIntegerField()
+    birth_date = models.DateField()
+    cpf = models.CharField(max_length=11, help_text='Just numbers.')
+    address = models.CharField(max_length=50)
+    number = models.CharField(max_length=5)
+    complement = models.CharField(max_length=30)
+    neightborhood = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8, help_text='Just numbers.')
+    city = models.CharField(max_length=30)
+    state = models.CharField(
+        max_length=2,
+        default='SP',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins')
+        )
+    )
+    
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    
+    
