58df4f841b81a5ccbc8369f00713dbb19c591a23
PedroCunha06 <pdh.alves@outlook.com>
2024-07-16 19:33:17 -0300

    Config model ProfileUser

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from user_profile.models import ProfileUser
 
-# Register your models here.
+@admin.register(ProfileUser)
+class ProfileUserAdmin(admin.ModelAdmin):
+    list_display = 'id', 'user', # Mostrar name no display
+    ordering = '-id', # Ordernar por Id decrescente
+    search_fields = 'name', 'id',  # Campo de pesquisa
+    list_per_page = 10  # Itens por página
+    list_max_show_all = 100 # Itens máximo a se mostrar na tela
+    list_display_links = 'user',   # O que está como editable, não pode estar aqui
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,51 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-'''
-PerfilUsuario
-        user - FK user (ou OneToOne)
-        idade - Int
-        data_nascimento - Date
-        cpf - char
-        endereco - char
-        numero - char
-        complemento - char
-        bairro - char
-        cep - Char
-        cidade - char
-        estado - Choices
-            ('AC', 'Acre'),
-            ('AL', 'Alagoas'),
-            ('AP', 'Amapá'),
-            ('AM', 'Amazonas'),
-            ('BA', 'Bahia'),
-            ('CE', 'Ceará'),
-            ('DF', 'Distrito Federal'),
-            ('ES', 'Espírito Santo'),
-            ('GO', 'Goiás'),
-            ('MA', 'Maranhão'),
-            ('MT', 'Mato Grosso'),
-            ('MS', 'Mato Grosso do Sul'),
-            ('MG', 'Minas Gerais'),
-            ('PA', 'Pará'),
-            ('PB', 'Paraíba'),
-            ('PR', 'Paraná'),
-            ('PE', 'Pernambuco'),
-            ('PI', 'Piauí'),
-            ('RJ', 'Rio de Janeiro'),
-            ('RN', 'Rio Grande do Norte'),
-            ('RS', 'Rio Grande do Sul'),
-            ('RO', 'Rondônia'),
-            ('RR', 'Roraima'),
-            ('SC', 'Santa Catarina'),
-            ('SP', 'São Paulo'),
-            ('SE', 'Sergipe'),
-            ('TO', 'Tocantins'),
-
-'''
-
 class ProfileUser(models.Model):
-    user = models.models.OneToOneField(User, on_delete=models.CASCADE)()
+    class Meta:
+        verbose_name = 'ProfileUser'   # O que mostrar quando houver uma categoria
+        verbose_name_plural = 'ProfileUsers'  # O que mostrar quando estiver no plural
+        
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.PositiveIntegerField()
     birth_date = models.DateField()
     cpf = models.CharField(max_length=11, help_text='Just numbers.')
@@ -91,6 +52,9 @@ class ProfileUser(models.Model):
     
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+    
+    def clean(self):
+        pass
     
     
     
