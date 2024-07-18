c520eddefae2bd69385def3d92e52d01a97b6ed7
PedroCunha06 <pdh.alves@outlook.com>
2024-07-18 11:38:26 -0300

    Fix errors

--- product/urls.py
+++ product/urls.py
@@ -6,7 +6,7 @@ app_name = 'product'
 # as.view(), quando se usa Class
 urlpatterns = [
     path('', views.ListProduct.as_view(), name='list'),
-    path('<slug>/', views.DetailsProduct.as_view(), name='detail' ),
+    path('product/<slug>/', views.DetailsProduct.as_view(), name='detail' ),
     path('addtocart/', views.AddToCart.as_view(), name='add_cart'),
     path('removetocart/', views.RemoveToCart.as_view(), name='remove_cart'),
     path('finish/', views.Finish.as_view(), name='finish'),
