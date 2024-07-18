450e28a53e77a062bcaef2f4b3b432dcfc1181f1
PedroCunha06 <pdh.alves@outlook.com>
2024-07-18 10:18:23 -0300

    Create page list products

--- product/views.py
+++ product/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
 
-class ListProduct(View):
-    ...
+from . import models
+
+class ListProduct(ListView):
+    model = models.Product
+    template_name = 'product/list.html'
+    context_object_name = 'products'
+    paginate_by = 9
 
 
 class DetailsProduct(View):
